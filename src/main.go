e25d775da5a2fed1a1abe15f4c8500c184360d17
Matheus Rossetti <matheuzdreher@gmail.com>
2026-03-17 19:54:45 -0300

    add small sleeps to make sure the logs from goroutines come out in the corret order

--- src/main.go
+++ src/main.go
@@ -7,6 +7,7 @@ import (
 	"os/signal"
 	"sync"
 	"syscall"
+	"time"
 
 	"github.com/Matheus-Rossetti/frevod/internal/core"
 	"github.com/Matheus-Rossetti/frevod/internal/downloader"
@@ -21,6 +22,11 @@ import (
 )
 
 func main() {
+	// HELLO!
+	// THIS IS THE MAIN FUNCTION OF FREVOD
+	// IT ORCHESTRATES EVERYTHING
+
+	// SLEEPS ARE SO LOGS COME OUT IN THE RIGHT ORDER
 
 	frevod := startFrevod(logger.Frevod())
 	frevod.CheckForFFmpegBin()
@@ -38,9 +44,19 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 
 	go func() {
+		// The order in which the queues are closed is important
+		// If we close encoder is closes when downloader finishes a download job
+		// the downloader will try to push the job to the encoder, fail and leave orphan files.
 		<-ctx.Done()
+		frevod.slog.Warn("Shutdown signal received!")
+
+		time.Sleep(time.Second / 2)
 		close(downloadQueue)
+
+		time.Sleep(time.Second / 2)
 		close(encodeQueue)
+
+		time.Sleep(time.Second / 2)
 		close(uploadQueue)
 	}()
 
@@ -48,6 +64,7 @@ func main() {
 
 	// START INPUT METHODS
 	if config.Input.UseTerminal {
+		time.Sleep(time.Second / 2)
 		log, slog := logger.Cli()
 		wg.Go(func() {
 			cli := cli.NewCli(ctx, log, slog, downloadQueue)
@@ -55,6 +72,7 @@ func main() {
 		})
 	}
 	if config.Input.UseREST {
+		time.Sleep(time.Second / 2)
 		log, slog := logger.Rest()
 		wg.Go(func() {
 			rest := rest.NewRest(ctx, log, slog, "8080", downloadQueue)
@@ -65,10 +83,12 @@ func main() {
 	// START OUTPUT METHODS
 	storageProviders := make(map[string]uploader.StorageProvider) // we pass storageProviders to uploader.Start
 	if config.Upload.Local.Use {
+		time.Sleep(time.Second / 2)
 		provider := local.Start(config)
 		storageProviders["local"] = provider
 	}
 	if config.Upload.S3.Use {
+		time.Sleep(time.Second / 2)
 		provider := s3.Start(config)
 		storageProviders["s3"] = provider
 	}
@@ -79,6 +99,7 @@ func main() {
 	frevod.FillFilePool(files, filePool)
 
 	// START DOWNLOADERS
+	time.Sleep(time.Second / 2)
 	for index := range config.Encode.ConcurrentEncodings * 2 {
 		wg.Go(func() {
 			log, slog := logger.Downloader()
@@ -88,15 +109,17 @@ func main() {
 	}
 
 	// START ENCODERS
+	time.Sleep(time.Second / 2)
 	for index := range config.Encode.ConcurrentEncodings {
-		wg.Add(1)
-		go func() {
-			encoder.Start(ctx, config, filePool, index, encodeQueue, uploadQueue)
-			wg.Done()
-		}()
+		wg.Go(func() {
+			log, slog := logger.Encoder()
+			encoder := encoder.NewEncoder(ctx, log, slog, config, filePool, index, encodeQueue, uploadQueue)
+			encoder.Start()
+		})
 	}
 
 	// START UPLOAD
+	time.Sleep(time.Second / 2)
 	for index := range config.Encode.ConcurrentEncodings {
 		wg.Add(1)
 		go func() {
