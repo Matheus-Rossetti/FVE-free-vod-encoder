d3101156b1f4fbe7af75919377791ea590f0c089
Matheus Rossetti <matheuzdreher@gmail.com>
2026-03-20 19:30:29 -0300

    Remove Cloudflare R2 credentials. I can't belive I've commited them

--- internal/options/parse.go
+++ internal/options/parse.go
@@ -30,11 +30,11 @@ func (o *options) ParseOptions() *core.Options {
 
 		Upload: core.UploadOptions{
 			S3: core.AmazonS3Options{
-				S3Endpoint:        "https://b169176ab0ecd53f481c3fc91eb60048.r2.cloudflarestorage.com",
-				S3AccessKey:       "98a0e3bb4a4650adc351b32ab6cde97c",
-				S3SecretAccessKey: "ac472e0373c488de2a2bb045011d744c34d830f14981dc151b0196d248131d12",
+				S3Endpoint:        "",
+				S3AccessKey:       "",
+				S3SecretAccessKey: "",
 				S3BucketName:      "videos",
-				Use:               true,
+				Use:               false,
 				S3UseSSL:          true,
 			},
 			Local: core.LocalOption{
