45f8296e0292c00fc6ae67563153989cd68be640
Matheus Rossetti <matheuzdreher@gmail.com>
2026-04-02 19:54:08 -0300

    refactor checkForVideo function, it's logic only now, it doesn't fetch data

--- internal/ingestor/download_to_file.go
+++ internal/ingestor/download_to_file.go
@@ -22,14 +22,6 @@ var (
 	ErrPointingToFileHead     = errors.New("failed to point to file's head")
 )
 
-type FileType int
-
-const (
-	unknown FileType = iota
-	notVideo
-	video
-)
-
 func (i *ingestor) prepareFileForDownload(file *os.File) error {
 
 	err := file.Truncate(0)
@@ -47,63 +39,24 @@ func (i *ingestor) prepareFileForDownload(file *os.File) error {
 }
 
 // Downloads the first 512 bytes (or less) and check fileType
-func (i *ingestor) checkFileType(url string) (FileType, error) {
-
-	requestContext, cancel := context.WithTimeout(i.ctx, time.Second*30)
-	defer cancel()
-
-	request, err := http.NewRequestWithContext(requestContext, "GET", url, nil)
-	if err != nil {
-		i.slog.Error(ErrFailedRequestCreation.Error(), "err", err)
-		return unknown, fmt.Errorf("%w: %v", ErrFailedRequestCreation, err)
-	}
-
-	request.Header.Set("Range", "bytes=0-511")
-
-	response, err := http.DefaultClient.Do(request)
-	if err != nil {
-		i.slog.Error(ErrFailedRequestExecution.Error(), "err", err)
-		return unknown, fmt.Errorf("%w: %v", ErrFailedRequestExecution, err)
-	}
-	defer response.Body.Close()
-
-	// The code below garantees that if the server ignores the Range in the Header and
-	// sends the full file, we cut the connection after downloading 512 bytes.
-	// This happens becuase of how http.Response.Body works.
-	// Hover ".Body" and check the first few lines in the docs, its awesome.
-	buffer := make([]byte, 512)
-	bytesRead, err := io.ReadFull(response.Body, buffer)
-	if err != nil {
-		i.slog.Error(ErrReadingBodyIntoBuffer.Error(), "err", err)
-		return unknown, fmt.Errorf("%w: %v", ErrReadingBodyIntoBuffer, err)
-	}
+func (i *ingestor) checkForVideo(bytes []byte) error {
 
 	// Need to pass :bytesRead in case less than 512 bytes were read into buffer
-	MIMEType := http.DetectContentType(buffer[:bytesRead])
+	MIMEType := http.DetectContentType(bytes)
 
 	fileType := strings.Split(MIMEType, "/")
 
 	if fileType[0] != "video" {
-		return notVideo, nil
+		return fmt.Errorf("file not a video")
 	}
 
-	return video, nil
+	return nil
 }
 
 func (i *ingestor) downloadToFile(url string, file *os.File) error {
 
-	// Check if it's a video
-	fyleType, err := i.checkFileType(url)
-	if err != nil {
-		return err
-	}
-	if fyleType != video {
-		i.slog.Error(ErrFileNotVideo.Error())
-		return ErrFileNotVideo
-	}
-
 	// Create request
-	requestCtx, cancel := context.WithTimeout(i.ctx, time.Minute*15)
+	requestCtx, cancel := context.WithTimeout(i.ctx, time.Minute*20)
 	defer cancel()
 
 	request, err := http.NewRequestWithContext(requestCtx, "GET", url, nil)
@@ -112,7 +65,6 @@ func (i *ingestor) downloadToFile(url string, file *os.File) error {
 		return fmt.Errorf("%w: %v", ErrFailedRequestCreation, err)
 	}
 
-	// Execute request
 	response, err := http.DefaultClient.Do(request)
 	if err != nil {
 		i.slog.Error(ErrFailedRequestExecution.Error(), "err", err)
@@ -124,6 +76,23 @@ func (i *ingestor) downloadToFile(url string, file *os.File) error {
 		i.slog.Error(ErrResponseStatusNotOk.Error(), "status", response.Status)
 		return fmt.Errorf("%w: status %v", ErrResponseStatusNotOk, response.Status)
 	}
+
+	// The code below garantees that if the server ignores the Range in the Header and
+	// sends the full file, we cut the connection after downloading 512 bytes.
+	// This happens becuase of how http.Response.Body works.
+	// Hover ".Body" and check the first few lines in the docs, its awesome.
+	buffer := make([]byte, 512)
+	bytesRead, err := io.ReadFull(response.Body, buffer)
+	if err != nil {
+		i.slog.Error(ErrReadingBodyIntoBuffer.Error(), "err", err)
+		return fmt.Errorf("%w: %v", ErrReadingBodyIntoBuffer, err)
+	}
+
+	err = i.checkForVideo(buffer[:bytesRead])
+	if err != nil {
+		return err
+	}
+	file.Write(buffer[:bytesRead])
 
 	// Download directly to file on disc
 	_, err = io.Copy(file, response.Body)
--- internal/ingestor/ingestor_test.go
+++ internal/ingestor/ingestor_test.go
@@ -1,97 +1,115 @@
 package ingestor
 
 import (
-	"net/http"
-	"net/http/httptest"
-	"os"
+	"context"
 	"testing"
 )
 
-func TestPrepareFileForDownload(t *testing.T) {
+// func TestPrepareFileForDownload(t *testing.T) {
+// 	testCases := []struct {
+// 		name      string
+// 		fileState string
+// 		wantErr   bool
+// 	}{
+// 		{
+// 			name:      "opened file",
+// 			fileState: "open",
+// 			wantErr:   false,
+// 		},
+// 		{
+// 			name:      "closed file",
+// 			fileState: "closed",
+// 			wantErr:   true,
+// 		},
+// 	}
+
+// 	for _, tt := range testCases {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			file, err := os.CreateTemp("", "temp-file-*")
+// 			if err != nil {
+// 				t.Errorf("failed to create a temp file to run tests with")
+// 			}
+// 			defer file.Close()
+// 			defer os.Remove(file.Name())
+
+// 			if tt.fileState == "closed" {
+// 				file.Close()
+// 			}
+
+// 			ingestor := ingestor{}
+
+// 			err = ingestor.prepareFileForDownload(file)
+
+// 			hasErr := (err != nil)
+// 			if tt.wantErr != hasErr {
+// 				t.Errorf("expected error to be %v, got %v", tt.wantErr, hasErr)
+// 				return // no need to continue test
+// 			}
+
+// 			meta, _ := file.Stat()
+// 			if meta.Size() != 0 {
+// 				t.Errorf("expected file size to be 0, got %v", meta.Size())
+// 			}
+
+// 			offSet, _ := file.Seek(0, 1)
+// 			if offSet != 0 {
+// 				t.Errorf("expected offset of file to be at 0, got %v", offSet)
+// 			}
+// 		})
+// 	}
+// }
+
+func TestCheckFileType(t *testing.T) {
+
 	testCases := []struct {
-		name      string
-		fileState string
-		wantErr   bool
+		name     string
+		fileType string
+		wantErr  bool
 	}{
 		{
-			name:      "opened file",
-			fileState: "open",
-			wantErr:   false,
+			name:     "video file type",
+			fileType: "video",
+			wantErr:  false,
 		},
 		{
-			name:      "closed file",
-			fileState: "closed",
-			wantErr:   true,
+			name:     "not video file type",
+			fileType: "not video",
+			wantErr:  true,
 		},
 	}
 
+	ingestor := ingestor{
+		ctx: context.Background(),
+	}
+
+	buffer := make([]byte, 512)
+
 	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			file, err := os.CreateTemp("", "temp-file-*")
-			if err != nil {
-				t.Errorf("failed to create a temp file to run tests with")
-			}
-			defer file.Close()
-			defer os.Remove(file.Name())
 
-			if tt.fileState == "closed" {
-				file.Close()
+			if tt.fileType == "video" {
+				buffer = videoMagicBytes()
+			} else {
+				buffer = textMagicBytes()
 			}
 
-			ingestor := ingestor{}
-
-			err = ingestor.prepareFileForDownload(file)
+			err := ingestor.checkForVideo(buffer)
 
 			hasErr := (err != nil)
 			if tt.wantErr != hasErr {
-				t.Errorf("expected error to be %v, got %v", tt.wantErr, hasErr)
-				return // no need to continue test
-			}
-
-			meta, _ := file.Stat()
-			if meta.Size() != 0 {
-				t.Errorf("expected file size to be 0, got %v", meta.Size())
-			}
-
-			offSet, _ := file.Seek(0, 1)
-			if offSet != 0 {
-				t.Errorf("expected offset of file to be at 0, got %v", offSet)
+				t.Errorf("Expected error to be %v, got %v",
+					tt.wantErr, hasErr)
 			}
 		})
 	}
 }
 
-func TestCheckFileType(t *testing.T) {
-
-	// mock server
-	server := httptest.NewServer(
-		http.HandlerFunc(
-			func(w http.ResponseWriter, r *http.Request) {
-
-			}))
-	defer server.Close()
-
-	testCases := []struct {
-		name    string
-		url     string
-		wantErr bool
-	}{
-		{
-			name:    "video file type",
-			url:     server.URL,
-			wantErr: false,
-		},
-		{
-			name:    "not video file type",
-			url:     server.URL,
-			wantErr: true,
-		},
-	}
-
-	for _, tt := range testCases {
-		t.Run(tt.name, func(t *testing.T) {
-
-		})
-	}
-
+// Files have something called "magic bytes", which are the first few bytes of a file
+// they are used to identify the file type.
+// Here are some examples: https://gist.github.com/iahu/396eaf109ed0969382abdbc9c3f0f029
+func videoMagicBytes() []byte {
+	return []byte{0x00, 0x00, 0x00, 0x18, 'f', 't', 'y', 'p', 'm', 'p', '4', '2'}
+}
+func textMagicBytes() []byte {
+	return []byte("This is not a videofile")
 }
