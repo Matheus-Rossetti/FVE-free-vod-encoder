6e230e221bce5e6e70bf8e72fc3144f1235ea80f
Matheus Rossetti <matheuzdreher@gmail.com>
2026-04-05 18:36:00 -0300

    add test cases for makeRequest function

--- internal/ingestor/ingestor_test.go
+++ internal/ingestor/ingestor_test.go
@@ -1,8 +1,13 @@
 package ingestor
 
 import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestPrepareFileForDownload(t *testing.T) {
@@ -27,11 +32,6 @@ func TestPrepareFileForDownload(t *testing.T) {
 
 			err = prepareFileForDownload(file)
 
-			hasErr := (err != nil)
-			if tt.wantErr != hasErr {
-				t.Errorf("expected error to be %v, got %v", tt.wantErr, hasErr)
-			}
-
 			meta, _ := file.Stat()
 			if meta.Size() != 0 {
 				t.Errorf("expected file size to be 0, got %v", meta.Size())
@@ -41,10 +41,73 @@ func TestPrepareFileForDownload(t *testing.T) {
 			if offSet != 0 {
 				t.Errorf("expected offset of file to be at 0, got %v", offSet)
 			}
+
+			hasErr := (err != nil)
+			if tt.wantErr != hasErr {
+				t.Errorf("expected error to be %v, got %v", tt.wantErr, hasErr)
+			}
 		})
 	}
 }
 
 // I couldn't write this test, so I'm just gonna leave it in god's hands
 func TestCheckBodyForVideo(t *testing.T) {
+}
+
+func TestMakeRequest(t *testing.T) {
+
+	testCases := []struct {
+		name       string
+		timeout    bool
+		statusCode int
+		wantErr    bool
+	}{
+		{
+			name:       "Successfuly crate and execute request",
+			timeout:    false,
+			statusCode: http.StatusOK,
+			wantErr:    false,
+		},
+		{
+			name:       "Status not 2**",
+			timeout:    false,
+			statusCode: http.StatusBadRequest,
+			wantErr:    true,
+		},
+		{
+			name:       "Request Timeout",
+			timeout:    true,
+			statusCode: http.StatusOK,
+			wantErr:    true,
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+
+			testServer := httptest.NewServer(
+				http.HandlerFunc(
+					func(w http.ResponseWriter, r *http.Request) {
+						time.Sleep(time.Millisecond * 20)
+						w.WriteHeader(tt.statusCode)
+						fmt.Fprint(w, "hello from the test server!")
+					},
+				),
+			)
+			defer testServer.Close()
+
+			ctx := context.Background()
+			if tt.timeout {
+				ctx, _ = context.WithTimeout(ctx, time.Millisecond*10)
+			}
+
+			_, err := makeRequest(testServer.URL, ctx)
+
+			hasErr := (err != nil)
+			if tt.wantErr != hasErr {
+				t.Errorf("expected error to be %v, got %v", tt.wantErr, hasErr)
+			}
+
+		})
+	}
 }
