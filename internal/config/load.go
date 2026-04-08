57aaed240d8f110bd967a16417f734eb23d6c82f
Matheus Rossetti <matheuzdreher@gmail.com>
2026-04-08 19:15:17 -0300

    add env var parsing when running in docker

--- internal/config/load.go
+++ internal/config/load.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/Matheus-Rossetti/frevod/internal/core"
 	"github.com/go-playground/validator/v10"
@@ -12,44 +13,50 @@ import (
 
 func Load(options *core.Options, path string) error {
 
-	err := loadStdOptions(options)
-	if err != nil {
-		return err
-	}
-
 	if isRunningInDocker() {
-		err := loadEnvVars(options)
-		if err != nil {
-			return err
-		}
+		fmt.Printf("\nRunning in Docker!")
+		fmt.Printf("\nLoading env vars...")
+
+		loadEnvVars(options)
+
+		fmt.Printf("\nEnv vars loaded!")
 		return nil
 	}
 
-	err = loadYaml(options, path)
+	err := loadYamlConfig(options, path)
 	if err != nil {
-		return err
+		fmt.Printf("\nCouldn't find config.yaml, using standard options...")
+		loadStdOptions(options)
 	}
 
 	// TODO improve validation logs
 	validate := validator.New()
 	err = validate.Struct(options)
 	if err != nil {
-		log.Fatal("Unvalid config\n", err)
+		log.Fatal("Invalid config\n", err)
 	}
 
 	return nil
 }
 
-func loadStdOptions(options *core.Options) error {
-	options.Encode.ConcurrentEncodings = 2
-	return nil
+// This is shit code, I know, I just wanna get this done already
+// I'll come back to it later... or so I hope...
+func loadEnvVars(options *core.Options) {
+	// ingest --------
+	options.Ingest.REST.Enabled, _ = strconv.ParseBool(os.Getenv("ENABLE_REST"))
+	options.Ingest.REST.Port = os.Getenv("ENABLE_CLI")
+	// encode --------
+	options.Encode.ConcurrentEncodings, _ = strconv.Atoi(os.Getenv("CONCURRENT_ENCODINGS"))
+	// dispatch ------
+	options.Dispatch.S3.Enabled, _ = strconv.ParseBool(os.Getenv("ENABLE_S3"))
+	options.Dispatch.S3.Bucket = os.Getenv("BUCKET")
+	options.Dispatch.S3.Endpoint = os.Getenv("ENDPOINT")
+	options.Dispatch.S3.AccessKey = os.Getenv("ACCESS_KEY")
+	options.Dispatch.S3.SecretAccessKey = os.Getenv("SECRET_ACCESS_KEY")
+	options.Dispatch.S3.UseSSL, _ = strconv.ParseBool(os.Getenv("USE_SSL"))
 }
 
-func loadEnvVars(options *core.Options) error {
-	return nil
-}
-
-func loadYaml(options *core.Options, path string) error {
+func loadYamlConfig(options *core.Options, path string) error {
 	if path == "" {
 		path = "config.yaml"
 	}
@@ -59,10 +66,25 @@ func loadYaml(options *core.Options, path string) error {
 		return fmt.Errorf("failed to read config file %q: %w", path, err)
 	}
 
+	fmt.Printf("\nFound config file!")
+	fmt.Printf("\nLoading config from yaml...")
+
 	err = yaml.Unmarshal(configFile, options)
 	if err != nil {
 		return fmt.Errorf("failed to parse yaml in %q: %w", path, err)
 	}
 
+	fmt.Printf("\nConfig loaded!")
+	return nil
+}
+
+func loadStdOptions(options *core.Options) error {
+	// ingest --------
+	options.Ingest.Cli.Enabled = true
+	// encode --------
+	options.Encode.ConcurrentEncodings = 2
+	// dispatch ------
+	options.Dispatch.Local.Enabled = true
+	options.Dispatch.Local.StoreAt = "."
 	return nil
 }
