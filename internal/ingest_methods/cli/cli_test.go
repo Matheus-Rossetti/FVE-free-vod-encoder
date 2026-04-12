7b3bff775de3c2803b3b1e5ac3d2d1a07d7b1712
Matheus Rossetti <matheuzdreher@gmail.com>
2026-04-11 22:32:11 -0300

    add integration test for the start function on cli package

--- internal/ingest_methods/cli/cli_test.go
+++ internal/ingest_methods/cli/cli_test.go
@@ -1,9 +1,13 @@
 package cli
 
 import (
+	"context"
+	"io"
+	"log"
 	"log/slog"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/Matheus-Rossetti/frevod/internal/core"
 )
@@ -18,7 +22,7 @@ func TestValidateInput(t *testing.T) {
 		wantErr            bool
 	}{
 		{
-			name:               "valid url and key",
+			name:               "twoTokens_http_ok",
 			input:              "http://coolvideo.com key/starter",
 			expectedUriType:    core.Url,
 			expectedUri:        "http://coolvideo.com",
@@ -26,7 +30,7 @@ func TestValidateInput(t *testing.T) {
 			wantErr:            false,
 		},
 		{
-			name:               "valid path and key",
+			name:               "twoTokens_path_ok",
 			input:              "/coolvideo.mp4 key/starter",
 			expectedUriType:    core.Path,
 			expectedUri:        "/coolvideo.mp4",
@@ -34,7 +38,7 @@ func TestValidateInput(t *testing.T) {
 			wantErr:            false,
 		},
 		{
-			name:               "with just uri (no key)",
+			name:               "oneToken_err",
 			input:              "http://coolvideo.com",
 			expectedUriType:    core.Url,
 			expectedUri:        "http://coolvideo.com",
@@ -42,7 +46,7 @@ func TestValidateInput(t *testing.T) {
 			wantErr:            true,
 		},
 		{
-			name:               "with more than url and key",
+			name:               "threeTokens_err",
 			input:              "http://coolvideo.com key/starter something-that-shouldn't-be-here",
 			expectedUriType:    core.Url,
 			expectedUri:        "http://coolvideo.com",
@@ -50,7 +54,7 @@ func TestValidateInput(t *testing.T) {
 			wantErr:            true,
 		},
 		{
-			name:               "with unsupported uri",
+			name:               "twoTokens_ftp_err",
 			input:              "ftp://coolvideo.com key/starter",
 			expectedUriType:    core.Unsupported,
 			expectedUri:        "ftp://coolvideo.com",
@@ -58,7 +62,7 @@ func TestValidateInput(t *testing.T) {
 			wantErr:            true,
 		},
 		{
-			name:               "with empty input",
+			name:               "empty_err",
 			input:              "",
 			expectedUriType:    core.Unsupported,
 			expectedUri:        "",
@@ -93,4 +97,65 @@ func TestValidateInput(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestStart_Integration(t *testing.T) {
+	t.Run("twoTokens_http_ok", func(t *testing.T) {
+		opts := core.NewOptions()
+		opts.Encode.ConcurrentEncodings = 1
+		ingest, _, _ := core.StartQueues(opts)
+
+		oldStdin := os.Stdin
+		r, w, err := os.Pipe()
+		if err != nil {
+			t.Fatal(err)
+		}
+		os.Stdin = r
+		t.Cleanup(func() {
+			os.Stdin = oldStdin
+			_ = r.Close()
+			_ = w.Close()
+		})
+
+		ctx, cancel := context.WithCancel(context.Background())
+		discardLog := log.New(io.Discard, "", 0)
+		discardSlog := slog.New(slog.NewTextHandler(io.Discard, nil))
+		c := NewCli(ctx, discardLog, discardSlog)
+
+		done := make(chan struct{})
+		go func() {
+			c.Start()
+			close(done)
+		}()
+
+		if _, err := w.WriteString("http://coolvideo.com key/starter\n"); err != nil {
+			t.Fatal(err)
+		}
+
+		select {
+		case job := <-ingest:
+			if job.DownloadJob.UriType != core.Url {
+				t.Errorf("UriType: got %v, want Url", job.DownloadJob.UriType)
+			}
+			if job.DownloadJob.VideoUri != "http://coolvideo.com" {
+				t.Errorf("VideoUri: got %q, want http://coolvideo.com", job.DownloadJob.VideoUri)
+			}
+			if job.UploadJob.KeyStarter != "key/starter" {
+				t.Errorf("KeyStarter: got %q, want key/starter", job.UploadJob.KeyStarter)
+			}
+			if job.DownloadJob.Source != "cli" {
+				t.Errorf("Source: got %q, want cli", job.DownloadJob.Source)
+			}
+		case <-time.After(3 * time.Second):
+			t.Fatal("timed out waiting for job on ingest queue")
+		}
+
+		cancel()
+
+		select {
+		case <-done:
+		case <-time.After(3 * time.Second):
+			t.Fatal("Start did not return after context cancel")
+		}
+	})
 }
